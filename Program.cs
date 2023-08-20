18371f352481b9e3d9b46780df2cca91b228c64a
rudolfm <rudolf.mendonca@gmail.com>
2023-08-21 07:10:00 +1000

    Class created

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using static LongestIncreasingSubsequence;
+
 int caseNumber = 1;
 string instructions = "Press Escape (Esc) to exit, Enter to see result";
 string inputTitle = "Please enter your numbers seperated by space.";
@@ -49,42 +50,44 @@ do
 
 } while (true);
 
-
-static int[] GetLongestIncreasingSubsequence(int[] numbers)
+public class LongestIncreasingSubsequence
 {
-    int[] lengths = new int[numbers.Length];
-    int[] previousIndices = new int[numbers.Length];
-    int maxLength = 0;
-    int lastIndex = -1;
-
-    for (int i = 0; i < numbers.Length; i++)
+    public static int[] GetLongestIncreasingSubsequence(int[] numbers)
     {
-        lengths[i] = 1;
-        previousIndices[i] = -1;
+        int[] lengths = new int[numbers.Length];
+        int[] previousIndices = new int[numbers.Length];
+        int maxLength = 0;
+        int lastIndex = -1;
 
-        for (int j = 0; j < i; j++)
+        for (int i = 0; i < numbers.Length; i++)
         {
-            if (numbers[i] > numbers[j] && lengths[j] + 1 > lengths[i])
+            lengths[i] = 1;
+            previousIndices[i] = -1;
+
+            for (int j = 0; j < i; j++)
             {
-                lengths[i] = lengths[j] + 1;
-                previousIndices[i] = j;
+                if (numbers[i] > numbers[j] && lengths[j] + 1 > lengths[i])
+                {
+                    lengths[i] = lengths[j] + 1;
+                    previousIndices[i] = j;
+                }
+            }
+
+            if (lengths[i] > maxLength)
+            {
+                maxLength = lengths[i];
+                lastIndex = i;
             }
         }
 
-        if (lengths[i] > maxLength)
+        var longestSubsequence = new int[maxLength];
+
+        for (int i = maxLength - 1; i >= 0; i--)
         {
-            maxLength = lengths[i];
-            lastIndex = i;
+            longestSubsequence[i] = numbers[lastIndex];
+            lastIndex = previousIndices[lastIndex];
         }
+
+        return longestSubsequence;
     }
-
-    var longestSubsequence = new int[maxLength];
-
-    for (int i = maxLength - 1; i >= 0; i--)
-    {
-        longestSubsequence[i] = numbers[lastIndex];
-        lastIndex = previousIndices[lastIndex];
-    }
-
-    return longestSubsequence;
 }
